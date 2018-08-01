cfd745ceaaf751a1e1588e36c26ac7a755c42f7f
Amee Covarrubias <ameecova@yahoo.com>
2018-08-01 18:33:41 +0000

    automatically backed up by learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,17 @@
-# Your code goes here!
+require 'pry'
+class Anagram
+  attr_reader :letters
+  
+  def initialize(string)
+    @letters = string
+  end 
+  
+  def match(arr)
+    arr. do |word| 
+      if word.split("").sort == @letters.split("").sort
+        return [word]
+      end
+    #binding.pry
+    end
+  end 
+end 
